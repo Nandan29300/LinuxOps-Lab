cc8daed6b5ac80fde0eb99dc2eba66e62b7a17d8
Nandan P Aghera <agheranandanp@gmail.com>
2026-06-08 15:24:01 +0530

    renamed folder names

--- users/delete-user.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-USERNAME=$1
-userdel -r $USERNAME
-echo "[$(date)] User '$USERNAME' deleted" >> reports/user-actions.log
-echo "✅ User '$USERNAME' deleted"
--- users/lock-user.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-USERNAME=$1
-passwd -l $USERNAME
-echo "[$(date)] User '$USERNAME' locked" >> reports/user-actions.log
-echo "🔒 User '$USERNAME' locked"
