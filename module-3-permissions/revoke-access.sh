c889fd3d4d1bd79b93a8ac4ed258f8ada8b17d5a
Nandan P Aghera <agheranandanp@gmail.com>
2026-06-09 15:26:33 +0530

    Update revoke-access.sh

--- module-3-permissions/revoke-access.sh
+++ module-3-permissions/revoke-access.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 FOLDER=$1
+
+if [ "$EUID" -ne 0 ]; then
+  echo "❌ Please run with sudo"
+  exit 1
+fi
+
 chmod -R 000 $FOLDER
 echo "[$(date)] Access revoked on $FOLDER" >> reports/permission-audit.log
 echo "🚫 Access revoked on $FOLDER"
