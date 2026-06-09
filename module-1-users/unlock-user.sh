1725b5190d9994d1c9482bcda1d0981c5a5e7525
Nandan P Aghera <agheranandanp@gmail.com>
2026-06-09 15:10:01 +0530

    Update unlock-user.sh

--- module-1-users/unlock-user.sh
+++ module-1-users/unlock-user.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 USERNAME=$1
+
+if [ "$EUID" -ne 0 ]; then
+  echo "❌ Please run with sudo"
+  exit 1
+fi
+
 passwd -u $USERNAME
 echo "[$(date)] User '$USERNAME' unlocked" >> reports/user-actions.log
 echo "🔓 User '$USERNAME' unlocked"
