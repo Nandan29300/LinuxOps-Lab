51b2320d4b344ad5320539ba572ffaa36d6a8f11
Nandan P Aghera <agheranandanp@gmail.com>
2026-06-09 15:26:21 +0530

    Update assign-user.sh

--- module-2-groups/assign-user.sh
+++ module-2-groups/assign-user.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 USERNAME=$1
 GROUP=$2
+
+if [ "$EUID" -ne 0 ]; then
+  echo "❌ Please run with sudo"
+  exit 1
+fi
+
 usermod -aG $GROUP $USERNAME
 echo "[$(date)] '$USERNAME' assigned to '$GROUP'" >> reports/user-actions.log
 echo "✅ '$USERNAME' added to '$GROUP'"
