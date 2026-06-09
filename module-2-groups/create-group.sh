f01516b76b9b22bf38bc3446161b448560f04086
Nandan P Aghera <agheranandanp@gmail.com>
2026-06-09 15:26:10 +0530

    Update create-group.sh

--- module-2-groups/create-group.sh
+++ module-2-groups/create-group.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 GROUPNAME=$1
+
+if [ "$EUID" -ne 0 ]; then
+  echo "❌ Please run with sudo"
+  exit 1
+fi
+
 groupadd $GROUPNAME
 echo "[$(date)] Group '$GROUPNAME' created" >> reports/user-actions.log
 echo "✅ Group '$GROUPNAME' created"
