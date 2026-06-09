856d508c970e831940e58fdd16494a2490fabd4a
Nandan P Aghera <agheranandanp@gmail.com>
2026-06-09 15:08:23 +0530

    Update create-user.sh

--- module-1-users/create-user.sh
+++ module-1-users/create-user.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# create-user.sh — Creates a new user and adds to a group
-
 USERNAME=$1
 GROUP=$2
 
@@ -9,16 +7,13 @@ if [ -z "$USERNAME" ] || [ -z "$GROUP" ]; then
   exit 1
 fi
 
-# Create group if it doesn't exist
+if [ "$EUID" -ne 0 ]; then
+  echo "❌ Please run with sudo"
+  exit 1
+fi
+
 groupadd -f $GROUP
-
-# Create user and add to group
 useradd -m -G $GROUP -s /bin/bash $USERNAME
-
-# Set default password same as username
 echo "$USERNAME:$USERNAME@123" | chpasswd
-
-# Log the action
 echo "[$(date)] User '$USERNAME' created and added to group '$GROUP'" >> reports/user-actions.log
-
 echo "✅ User '$USERNAME' created and added to group '$GROUP'"
