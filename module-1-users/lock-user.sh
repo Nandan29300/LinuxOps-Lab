f1ed18701f77adbcca387dfc08240f2a564ebb91
Nandan P Aghera <agheranandanp@gmail.com>
2026-06-09 15:09:33 +0530

    Update lock-user.sh

--- module-1-users/lock-user.sh
+++ module-1-users/lock-user.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 USERNAME=$1
+
+if [ "$EUID" -ne 0 ]; then
+  echo "❌ Please run with sudo"
+  exit 1
+fi
+
 passwd -l $USERNAME
 echo "[$(date)] User '$USERNAME' locked" >> reports/user-actions.log
 echo "🔒 User '$USERNAME' locked"
