33924fc812207753b71ef5df5ec292c8441cf908
Nandan P Aghera <agheranandanp@gmail.com>
2026-06-09 15:08:42 +0530

    Update delete-user.sh

--- module-1-users/delete-user.sh
+++ module-1-users/delete-user.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 USERNAME=$1
-userdel -r $USERNAME
+
+if [ "$EUID" -ne 0 ]; then
+  echo "❌ Please run with sudo"
+  exit 1
+fi
+
+userdel -r $USERNAME 2>/dev/null
 echo "[$(date)] User '$USERNAME' deleted" >> reports/user-actions.log
 echo "✅ User '$USERNAME' deleted"
